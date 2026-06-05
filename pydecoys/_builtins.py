e724d190897afc5e2a00fa00847558d14a85d647
brunomaestri <bruno.maestri@edu.pucrs.br>
2026-06-05 14:55:17 -0300

    refactor: Improve strategy registration logic so it's cleaner

--- pydecoys/_builtins.py
+++ pydecoys/_builtins.py
@@ -17,10 +17,12 @@
 
 """Internal logic for implemented strategies."""
 
-import itertools
-from typing import Iterable, Literal, cast
+from typing import Callable, Iterable, Literal, cast
 
 from pydecoys import strategies as s
+
+
+decoy_strategy: dict[str, s.DecoyGenerator] = {}
 
 
 def reverse[T: s.SeqLike](sequence: T) -> T:
@@ -28,35 +30,11 @@ def reverse[T: s.SeqLike](sequence: T) -> T:
     return cast(T, sequence[::-1])
 
 
-reverse_keepn = s.keepsn(reverse)
-"""Return the reversed `sequence`, except N-terminal aa."""
-
-
-reverse_keepc = s.keepsc(reverse)
-"""Return the reversed `sequence`, except C-terminal aa."""
-
-
-reverse_keepterm = s.keepsterm(reverse)
-"""Return the reversed `sequence`, except terminal aas."""
-
-
 def shuffle[T: s.SeqLike](sequence: T) -> T:
     """Return the shuffled `sequence`."""
     new = list(sequence)
     s.RAND.shuffle(new)
     return s.seq_cast(sequence, "".join(new))
-
-
-shuffle_keepn = s.keepsn(shuffle)
-"""Return the shuffled `sequence`, except N-terminal aa."""
-
-
-shuffle_keepc = s.keepsc(shuffle)
-"""Return the shuffled `sequence`, except N-terminal aa."""
-
-
-shuffle_keepterm = s.keepsterm(shuffle)
-"""Return the shuffled `sequence`, except terminal aas."""
 
 
 class Randomize:
@@ -87,110 +65,76 @@ class Randomize:
         return s.seq_cast(sequence, "".join(new))
 
 
-randomize = Randomize()
-randomize_keepn = s.keepsn(Randomize())
-randomize_keepc = s.keepsc(Randomize())
-randomize_keepterm = s.keepsterm(Randomize())
-
-
-decoy_strategy: dict[str, s.DecoyGenerator] = {
-    "reverse": reverse,
-    "reverse-keepn": reverse_keepn,
-    "reverse-keepc": reverse_keepc,
-    "reverse-keepterm": reverse_keepterm,
-    "shuffle": shuffle,
-    "shuffle-keepn": shuffle_keepn,
-    "shuffle-keepc": shuffle_keepc,
-    "shuffle-keepterm": shuffle_keepterm,
-    "randomize": randomize,
-    "randomize-keepn": randomize_keepn,
-    "randomize-keepc": randomize_keepc,
-    "randomize-keepterm": randomize_keepterm,
-}
-
-
 # Pre-defined enzymes
 # Those specifications were taken from <https://github.com/HUPO-PSI/psi-ms-CV>
+#
+# (regex, cleavage sense, str key)
+# The regex patterns MUST ONLY match the cleavage site and MUST capture it.
+# Because of this, we cannot use the regex patterns from PSI MS Ontology.
 
-type _Enzyme = tuple[str, str | None, str | None, Literal['N', 'C', 'both'], str]
+type _Enzyme = tuple[str, Literal['N', 'C', 'both'], str]
 
-_ALPHA_LP: _Enzyme = ('TASV', None, None, 'C', 'alphalp')
-_ARG_C: _Enzyme = ('R', 'P', None, 'C', 'argc')
-_ASP_N: _Enzyme = ('BD', None, None, 'N', 'aspn')
-_ASP_N_AMBIC: _Enzyme = ('DE', None, None, 'N', 'aspnambic')
-_CHYMO: _Enzyme = ('FYWL', 'P', None, 'C', 'chymo')
-_CNBR: _Enzyme = ('M', None, None, 'C', 'cnbr')
-_FORMIC_ACID: _Enzyme = ('D', None, None, 'both', 'formicacid')
-_GLUTAMYL_ENDOPEPTIDASE: _Enzyme = ('E', None, 'E', 'C', 'gluc')
-_LEUKOCYTE_ELASTASE: _Enzyme = ('ALIV', 'P', None, 'C', 'elastase')
-_LYS_C: _Enzyme = ('K', 'P', None, 'C', 'lysc')
-_LYS_C_P: _Enzyme = ('K', None, None, 'C', 'lyscp')
-_LYS_N: _Enzyme = ('K', None, None, 'N', 'lysn')
-_PEPSIN_A: _Enzyme = ('FL', None, None, 'C', 'pepsina')
-_TRYPSIN: _Enzyme = ('KR', 'P', None, 'C', 'trypsin')
-_TRYPSIN_P: _Enzyme = ('KR', None, None, 'C', 'trypsinp')
-_TRYP_CHYMO: _Enzyme = ('FYWLKR', 'P', None, 'C', 'trypchymo')
-_TRYP_N: _Enzyme = ('KR', None, None, 'N', 'trypn')
-_TWO_IODOBENZOATE: _Enzyme = ('W', None, None, 'C', '2iodobenzoate')
-_V8_DE: _Enzyme = ('BDEZ', 'P', None, 'C', 'v8de')
-_V8_E: _Enzyme = ('EZ', 'P', None, 'C', 'v8e')
-
-# Pro-C is the only one that needs a regex pattern
-_PROLINE_ENDOPEPTIDASE = (r'([HKR]P)(?!P)', 'proc')
-
-
-def _register_enzymatic_strategies():
-    strategies = itertools.product(
-        [s.ReversePep, s.ShufflePep, s.RandomizePep],
-        [
-            _ALPHA_LP,
-            _ARG_C,
-            _ASP_N,
-            _ASP_N_AMBIC,
-            _CHYMO,
-            _CNBR,
-            _FORMIC_ACID,
-            _GLUTAMYL_ENDOPEPTIDASE,
-            _LEUKOCYTE_ELASTASE,
-            _LYS_C,
-            _LYS_C_P,
-            _LYS_N,
-            _PEPSIN_A,
-            _TRYPSIN,
-            _TRYPSIN_P,
-            _TRYP_CHYMO,
-            _TRYP_N,
-            _TWO_IODOBENZOATE,
-            _V8_DE,
-            _V8_E,
-            None,
-        ],
-        ['keepsn', 'keepsc', 'keepsterm', None],
-    )
-
-    # TODO: Find a better way of coding this
-    # Maybe exposing a function that automatically registers all combinations
-    # of an enzyme/EnzymeSpecificGenerator might be useful for plug-ins?
-    # Not make it automatic so plug-ins can opt out of it?
-    for factory, enzyme, term in strategies:
-        if enzyme is None:
-            key = factory.__name__.lower() + '-' + _PROLINE_ENDOPEPTIDASE[1]
-            generator = factory(_PROLINE_ENDOPEPTIDASE[0])
-        else:
-            key = factory.__name__.lower() + '-' + enzyme[4]
-            generator = factory.from_enzyme(enzyme[0], enzyme[1], enzyme[2], enzyme[3])
-
-        match term:
-            case 'keepsn':
-                generator = s.keepsn(generator)
-                key += '-keepn'
-            case 'keepsc':
-                generator = s.keepsc(generator)
-                key += '-keepc'
-            case 'keepsterm':
-                generator = s.keepsterm(generator)
-                key += '-keepterm'
-        decoy_strategy[key] = generator
+_ENZYMES: list[_Enzyme] = [
+    (r'([TASV])',        'C',    'alphalp'),        # AlphaLP
+    (r'(R)(?!P)',        'C',    'argc'),           # Arg-C
+    (r'([BD])',          'N',    'aspn'),           # Asp-N
+    (r'([DE])',          'N',    'aspnambic'),      # Asp-N_ambic
+    (r'([FYWL])(?!P)',   'C',    'chymo'),          # Chymotrypsin
+    (r'(M)',             'C',    'cnbr'),           # CNBr
+    (r'(D)',             'both', 'formicacid'),     # Formic_acid
+    (r'(?<!E)(E)',       'C',    'gluc'),           # glutamyl endopeptidase
+    (r'([ALIV])(?!P)',   'C',    'elastase'),       # leukocyte elastase
+    (r'(K)(?!P)',        'C',    'lysc'),           # Lys-C
+    (r'(K)',             'C',    'lyscp'),          # Lys-C/P
+    (r'(K)',             'N',    'lysn'),           # Lys-N
+    (r'([FL])',          'C',    'pepsina'),        # PepsinA
+    (r'([HKR]P)(?!P)',   'C',    'proc'),           # proline endopeptidase
+    (r'([KR])(?!P)',     'C',    'trypsin'),        # Trypsin
+    (r'([KR])',          'C',    'trypsinp'),       # Trypsin/P
+    (r'([FYWLKR])(?!P)', 'C',    'trypchymo'),      # TrypChymo
+    (r'([KR])',          'N',    'trypn'),          # Tryp-N
+    (r'(W)',             'C',    '2iodobenzoate'),  # 2-iodobenzoate
+    (r'([BDEZ])(?!P)',   'C',    'v8de'),           # V8-DE
+    (r'([EZ])(?!P)',     'C',    'v8e'),            # V8-E
+]
 
 
-_register_enzymatic_strategies()
+_PLAIN_STRATEGIES: list[tuple[str, Callable[[], s.DecoyGenerator]]] = [
+    ('reverse',   lambda: reverse),  # noqa: E272
+    ('shuffle',   lambda: shuffle),  # noqa: E272
+    ('randomize', Randomize)
+]
+
+
+_ENZYME_STRATEGIES: list[tuple[str, type[s.EnzymeSpecificGenerator]]] = [
+    ('reversepep',   s.ReversePep),
+    ('shufflepep',   s.ShufflePep),
+    ('randomizepep', s.RandomizePep)
+]
+
+
+_TERMINAL_FACTORIES: list[
+    tuple[str, Callable[[s.DecoyGenerator], s.DecoyGenerator]]
+] = [
+    ('keepn',    s.keepsn),
+    ('keepc',    s.keepsc),
+    ('keepterm', s.keepsterm)
+]
+
+
+def _register_all() -> None:
+    for base_key, make in _PLAIN_STRATEGIES:
+        decoy_strategy[base_key] = make()
+        for suffix, factory in _TERMINAL_FACTORIES:
+            decoy_strategy[f'{base_key}-{suffix}'] = factory(make())
+
+    for class_key, cls in _ENZYME_STRATEGIES:
+        for regex, sense, enzyme_key in _ENZYMES:
+            base_key = f'{class_key}-{enzyme_key}'
+            decoy_strategy[base_key] = cls(regex, sense)
+
+            for suffix, factory in _TERMINAL_FACTORIES:
+                decoy_strategy[f'{base_key}-{suffix}'] = factory(cls(regex, sense))
+
+
+_register_all()
